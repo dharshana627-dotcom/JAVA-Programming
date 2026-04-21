0e093e723812688e6967e86a58e3255c553fe000
Dharshana Ganesan <dharshana627@gmail.com>
2026-04-21 19:58:53 +0530

    Add files via upload

---
+++ String.java/SQL11.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class SQL11{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine(); 
+        System.out.println("Sanitized Inputs:");
+        for (int i = 1; i <= n; i++) {
+            String input = sc.nextLine();
+            String sanitized = input.replace("'", "''");
+            System.out.println(i + ". " + input + " -> " + sanitized);
+        }
+    }
+}
