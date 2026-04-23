3ee660448caae12b02428300c100686d21e15152
Dharshana Ganesan <dharshana627@gmail.com>
2026-04-23 22:04:15 +0530

    Add files via upload

---
+++ String.java/Genome13.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Genome13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        System.out.println("RNA Transcription:");
+        for (int i = 1; i <= n; i++) {
+            String dna = sc.nextLine();
+            String rna = dna.replace('T', 'U');
+            System.out.println(i + ". " + dna + " -> " + rna);
+        }
+    }
+}
