10dd7361a29918aa8746473b3904af3458bb2f25
Dharshana Ganesan <dharshana627@gmail.com>
2026-05-01 17:39:30 +0530

    Add files via upload

---
+++ String.java/Morse21.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Morse21{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        Map<String, String> map = new HashMap<>();
+        map.put(".-", "A"); map.put("-...", "B"); map.put("-.-.", "C");
+        map.put("-..", "D"); map.put(".", "E"); map.put("..-.", "F");
+        map.put("--.", "G"); map.put("....", "H"); map.put("..", "I");
+        map.put(".---", "J"); map.put("-.-", "K"); map.put(".-..", "L");
+        map.put("--", "M"); map.put("-.", "N"); map.put("---", "O");
+        map.put(".--.", "P"); map.put("--.-", "Q"); map.put(".-.", "R");
+        map.put("...", "S"); map.put("-", "T"); map.put("..-", "U");
+        map.put("...-", "V"); map.put(".--", "W"); map.put("-..-", "X");
+        map.put("-.--", "Y"); map.put("--..", "Z");
+        String result = "";
+        String words[] = input.split(" / ");
+        for (int i = 0; i < words.length; i++) {
+            String letters[] = words[i].split(" ");
+            for (String code : letters) {
+                result += map.getOrDefault(code, "");
+            }
+            if (i < words.length - 1) {
+                result += " ";
+            }
+        }
+        System.out.println("English Translation: " + result);
+    }
+}
