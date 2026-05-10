7634b39904eb668a0f8639d777d19504a0a2e598
Dharshana Ganesan <dharshana627@gmail.com>
2026-05-10 09:10:40 +0530

    Add files via upload

---
+++ While_DoWhile.java/CollatzSequence15.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class CollatzSequence15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int steps = 0;
+        System.out.println("Collatz sequence starting from " + n + ":");
+        do {
+            System.out.print(n + " ");
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = 3 * n + 1;
+            }
+            steps++;
+        } while (n != 1);
+        System.out.print("1");
+        System.out.println("\nSteps: " + steps);
+    }
+}
